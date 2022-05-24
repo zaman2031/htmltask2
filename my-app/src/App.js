7feb3311928459fe3570fdadf579f6f9bc4ced6b
Zaman Mehmood <muhammad.zaman@southvillesol.com>
2022-05-24 14:58:41 +0500

    Remove extra files &  extra spaces and remove commented code ^

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,14 +1,10 @@
 import React from 'react';
 import NavBar from './components/Navbar';
-import Profile from './components/Profile';
-import Hope from './components/hope';
-
+ 
 function App() {
   return (
     <div className="App">
-       {/* <NavBar /> */}
-       <Profile />
-       {/* <Hope /> */}
+       <NavBar />
     </div>
   );
 }
