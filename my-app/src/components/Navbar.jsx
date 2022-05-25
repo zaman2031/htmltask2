f46851b9606c52f38f20e4daef1fe377406b3655
Zaman Mehmood <muhammad.zaman@southvillesol.com>
2022-05-25 15:01:01 +0500

    Resolve comments

--- my-app/src/components/Navbar.jsx
+++ my-app/src/components/Navbar.jsx
@@ -1,27 +1,25 @@
 import React from 'react';
-import logo from '../Logos/wecare-logo.svg'
+import logo from '../assets/wecare-logo.svg'
 import './Navbar.scss';
 
 const Navbar = () => {
     return (
-            <div className='main-container'>
-                <div className='wecare-logo'>
-                    <img src={logo} alt="" />
-                </div>
-                <nav>
-                    <ul>
-                        <li>Job search</li>
-                        <li>be on one's mind!</li>
-                        <li>message</li>
-                        <li className='list-item'>My page</li>
-                        <li className='N-character'>N</li>
-                    </ul>
-                    <button className='logout-button'>Logout</button>
-                </nav>
+        <div className='main-container'>
+            <div className='main-logo'>
+                <img src={logo} alt="This is logo of company" />
             </div>
+            <nav>
+                <ul>
+                    <li>Job search</li>
+                    <li>be on one's mind!</li>
+                    <li>message</li>
+                    <li className='list-item'>My page</li>
+                    <li className='list-styling'>N</li>
+                </ul>
+                <button className='logout-button'>Logout</button>
+            </nav>
+        </div>
     );
 }
 
 export default Navbar;
-
- 
