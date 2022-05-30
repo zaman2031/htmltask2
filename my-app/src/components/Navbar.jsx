c7258428a890be9ab9e2832f1a476ddaea1a266c
Zaman Mehmood <muhammad.zaman@southvillesol.com>
2022-05-30 16:48:50 +0500

    nav is done

--- my-app/src/components/Navbar.jsx
+++ my-app/src/components/Navbar.jsx
@@ -5,19 +5,19 @@ import './Navbar.scss';
 const Navbar = () => {
     return (
         <div className='main-container'>
-            <img className='main-logo' src={logo} alt="wecare-Logo"  width="190" height="100" />
+            <img className='main-logo' src={logo} alt="wecare-Logo"  width="190" height="75" />
             <nav>
                 <ul>
                     <li>Job search</li>
                     <li>be on one's mind!</li>
                     <li>message</li>
-                    <li className='list-item'>My page</li>
+                    <li className='list-item'>My page</li>  
                     <li className='user-main'>N</li>
                 </ul>
                 <button className='logout-button'>Logout</button>
             </nav>
         </div>
     );
-}
+} 
 
 export default Navbar;
