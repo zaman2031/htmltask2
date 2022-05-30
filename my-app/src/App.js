29f3ab5ce2de7430adb4b6a16784efad1b7f0c77
Zaman Mehmood <muhammad.zaman@southvillesol.com>
2022-05-30 11:44:20 +0500

    Hope section is ready

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Navbar from './components/Navbar';
 import Profile from './components/Profile';
+import Hope from './components/hope';
 
 function App() {
   return (
     <div className="App">
        <Navbar />
        <Profile />
+       <Hope />
     </div>
   );
 }
