3a9367364d66228bbe9986beee8fcb9fe66a5653
Zaman Mehmood <muhammad.zaman@southvillesol.com>
2022-05-31 19:32:02 +0500

    Profile code is changed

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import Navbar from './components/Navbar';
+import Profile from './components/Profile';
+import Hope from './components/hope';
 import Info from './components/info';
- 
+
 function App() {
   return (
     <div className="App">
+       <Navbar />
+       <Profile />
+       <Hope />
        <Info />
     </div>
   );
