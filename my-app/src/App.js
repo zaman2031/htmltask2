42698f40634535f966f6c047c2c01baf95e269b3
Zaman Mehmood <muhammad.zaman@southvillesol.com>
2022-06-02 17:01:15 +0500

    Remove extra files from navbar component

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,16 +1,10 @@
 import React from 'react';
 import Navbar from './components/Navbar';
-import Profile from './components/Profile';
-import Hope from './components/hope';
-import Info from './components/info';
 
 function App() {
   return (
     <div className="App">
        <Navbar />
-       <Profile />
-       <Hope />
-       <Info />
     </div>
   );
 }
